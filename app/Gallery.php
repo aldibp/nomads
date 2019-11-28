06366201b30f389ff0f2e72ed12fa39b48eeab41
aldibp <aldibp97@gmail.com>
2019-11-28 19:01:50 +0700

    crud travelpackage

---
+++ app/Gallery.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Gallery extends Model
+{
+    use SoftDeletes;
+
+    protected $fillable = [
+        'travel_packages_id', 'image',
+    ];
+
+    protected $hidden = [
+
+    ];
+}
